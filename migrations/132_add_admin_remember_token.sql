de84be77e9c422fccd38959c60fe77c76014b8f1
moroccanrugsblankets <moroccanrugsblankets@gmail.com>
2026-04-04 14:37:32 +0100

    Comment out remember_token column and index creation
    Comment out the addition of remember_token column and index.

--- migrations/132_add_admin_remember_token.sql
+++ migrations/132_add_admin_remember_token.sql
@@ -1,7 +1,7 @@
 -- Migration 132: Add remember_token column to administrateurs table
 -- This column stores a secure token hash for the "Rester connecté" feature
 
-ALTER TABLE administrateurs 
-    ADD COLUMN remember_token VARCHAR(64) NULL DEFAULT NULL COMMENT 'Token for persistent login cookie';
+-- ALTER TABLE administrateurs 
+ --    ADD COLUMN remember_token VARCHAR(64) NULL DEFAULT NULL COMMENT 'Token for persistent login cookie';
 
-CREATE INDEX  IF NOT EXISTS idx_admin_remember_token ON administrateurs(remember_token);
+-- CREATE INDEX  IF NOT EXISTS idx_admin_remember_token ON administrateurs(remember_token);
