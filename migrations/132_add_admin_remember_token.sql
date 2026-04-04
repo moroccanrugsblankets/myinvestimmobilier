8dd8b0aeee667cafa941b287e6189d696a8ebce7
moroccanrugsblankets <moroccanrugsblankets@gmail.com>
2026-04-04 01:28:23 +0100

    Remove IF NOT EXISTS from remember_token column addition

--- migrations/132_add_admin_remember_token.sql
+++ migrations/132_add_admin_remember_token.sql
@@ -2,6 +2,6 @@
 -- This column stores a secure token hash for the "Rester connecté" feature
 
 ALTER TABLE administrateurs 
-    ADD COLUMN IF NOT EXISTS remember_token VARCHAR(64) NULL DEFAULT NULL COMMENT 'Token for persistent login cookie';
+    ADD COLUMN remember_token VARCHAR(64) NULL DEFAULT NULL COMMENT 'Token for persistent login cookie';
 
 CREATE INDEX IF NOT EXISTS idx_admin_remember_token ON administrateurs(remember_token);
